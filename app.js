fdcfc99286fa4056193dad181bb2fe9a17206bb8
Akshiv20 <queenprince20@gmail.com>
2026-01-10 11:34:36 +0530

    new updated code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome! CI/CD Started'));
+app.get('/', (req, res) => res.send('Welcome! AWS CI/CD Done'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
