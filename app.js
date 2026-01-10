b465fd58a7e682a47055149d67b2a9821b8e6907
Akshiv20 <queenprince20@gmail.com>
2026-01-10 10:48:45 +0530

    new CI/CD started

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome! CI/CD Done'));
+app.get('/', (req, res) => res.send('Welcome! CI/CD Started'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
